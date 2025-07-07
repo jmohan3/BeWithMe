35e348b0c1311113d3836a7741864bd96474a5eb
Jagan <jaganmohan.ios3@gmail.com>
2025-07-07 11:24:10 +0530

    Setting intial app setup

---
+++ BeWithMe/BeWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeWithMeApp.swift
+//  BeWithMe
+//
+//  Created by Kudumala on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BeWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
