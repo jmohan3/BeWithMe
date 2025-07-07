7ba6a2218bd50ebc6c76f79a3a4941af285c5787
Jagan <jaganmohan.ios3@gmail.com>
2025-07-07 12:06:03 +0530

    Adding assets and constants

---
+++ BeWithMe/Constants/Constants.swift
@@ -0,0 +1,62 @@
+//
+//  Constants.swift
+//  BeWithMe
+//
+//  Created by Kudumala on 07/07/25.
+//
+
+import Foundation
+
+enum AppHeaders: String {
+    case makeFriends = "Make Friends"
+    case searchFriends = "Search Friends"
+    case discover = "Discover"
+    case matches = "Matches"
+    case intrests = "Interests"
+    case discoverByInterests = "Discover By Interests"
+    case aroundMe    = "Around Me"
+    case messages    = "Messages"
+    case filters    = "Filters"
+    case location    = "Location"
+    case distance    = "Distance"
+    case age         = "Age"
+    case onlineNow   = "Online Now"
+    case preferences  = "Preferences"
+    case gender      = "Gender"
+    case height      = "Height"
+    case weight      = "Weight"
+    case maritalStatus  = "Marital Status"
+    case recentMatches  = "Recent Matches"
+    case uploadPhotos  = "Upload Photos"
+    case away        = "km away"
+    case match       = "% match"
+    case yourMatches  = "Your Matches"
+    case likes        = "Likes"
+    case views        = "Views"
+    case connect     = "Connect"
+    case interestedIn  = "Interested In"
+    case people      = "People"
+    case new         = "New"
+    case connectWith  = "Connect With"
+    case ago         = "ago"
+    
+}
+
+enum ButtonTitles: String {
+    case filter = "Filter"
+    case login = "Login"
+    case signup = "Signup"
+    case restorePassword = "Restore Password"
+    case donthaveAccount = "Don't have an account?"
+    case add = "+Add"
+    case remove = "Remove"
+    case music = "🎶 Music"
+    case fashion = "👗 Fashion"
+    case nature = "🪴 Nature"
+    case photography = "📸 Photography"
+    case football = "⚽️ Football"
+    case food = "🍽️ Food"
+    case writting = "✍️ Writting"
+    case viewAll = "View All"
+    
+}
